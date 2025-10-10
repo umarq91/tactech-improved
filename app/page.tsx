faf05c89d6e077567cddf7d66bf235ee87e253cc
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-10-10 18:46:36 +0500

    testing by cleaning page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 import { HeroSection } from "@/components/sections/hero-section";
-import { ContactSection } from "@/components/sections/contact-section";
-import ProjectGsap from "@/components/projectsgsap";
-import { HorizontalServices } from "@/components/HorizontalShowcase";
-import { BentoDemo } from "@/components/sections/Bento";
 
 export default function Home() {
   return (
     <div className="relative w-full overflow-x-hidden bg-pattern">
       <HeroSection />
-
-      <HorizontalServices />
-      <BentoDemo />
-      <ProjectGsap showmore={true} />
-      {/* <ServicesSection /> */}
-      <ContactSection />
     </div>
   );
 }
