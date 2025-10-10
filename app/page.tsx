7bac383ef88185ef2ce5e61302cf371b2948ec1a
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-10-10 19:41:43 +0500

    testing menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { HeroSection } from "@/components/sections/hero-section";
 import { ContactSection } from "@/components/sections/contact-section";
 import ProjectGsap from "@/components/projectsgsap";
 import { HorizontalServices } from "@/components/HorizontalShowcase";
+import Menu from "@/components/custom/menu";
 
 export default function Home() {
   return (
     <div className="relative w-full overflow-x-hidden">
+      <Menu />
       <HeroSection />
 
       <HorizontalServices />
